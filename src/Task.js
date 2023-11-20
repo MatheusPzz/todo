e5db49013d4220f90c158abed81c0f90dc123028
MatheusPzz <matheusperazzo2014@hotmail.com>
2023-11-20 10:44:52 +0000

    "First commit"

---
+++ src/Task.js
@@ -0,0 +1,13 @@
+export const Task = (props) => {
+    return (
+        <div 
+                className="Task"
+                style={ {backgroundColor: props.completed ? "green" : "pink" } }>
+                <h1>{props.taskName}</h1>
+        
+                <button onClick={() => props.completeTask(props.id)}> Complete </button>
+        
+                <button onClick={() => props.deleteTask(props.id)}> X </button>
+        </div>
+            );
+    }
